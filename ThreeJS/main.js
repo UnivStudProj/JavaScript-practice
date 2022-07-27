e38bcb488463eba1a92e0f5f1ef48d88a9cf2b5a
qJay44 <m70155302@yandex.ru>
2022-07-27 15:15:15 +0300

    create a scene, camera and renderer

--- ThreeJS/main.js
+++ ThreeJS/main.js
@@ -1,4 +1,9 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-console.log(scene);
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(innerWidth, innerHeight);
+
+document.body.appendChild(renderer.domElement);
