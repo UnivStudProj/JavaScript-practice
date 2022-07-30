b5b527750f3acac7f3fcc2bbacbb525a103b589c
qJay44 <m70155302@yandex.ru>
2022-07-30 15:43:28 +0300

    Add a plane mesh

--- ThreeJS/main.js
+++ ThreeJS/main.js
@@ -9,17 +9,16 @@ renderer.setPixelRatio(devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00FF00 });
-const mesh = new THREE.Mesh(boxGeometry, material);
-
-scene.add(mesh);
 camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000, side: THREE.DoubleSide });
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+
+scene.add(planeMesh);
 
 function animate() {
     renderer.render(scene, camera);
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
 
     requestAnimationFrame(animate);
 }
