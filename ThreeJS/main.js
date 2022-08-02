8e7635ed8e5a3ce7c61e0b3e2c253b5a5a110e6c
qJay44 <m70155302@yandex.ru>
2022-08-02 16:14:24 +0300

    dat.gui

--- ThreeJS/main.js
+++ ThreeJS/main.js
@@ -1,4 +1,39 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
+
+const gui = new dat.GUI();
+const world = {
+    plane: {
+        width: 10,
+        height: 10,
+        widthSegments: 10,
+        heightSegments: 10
+    }
+}
+
+gui.add(world.plane, 'width', 1, 20).onChange(generatePlane);
+gui.add(world.plane, 'height', 1, 20).onChange(generatePlane);
+gui.add(world.plane, 'widthSegments', 1, 50).onChange(generatePlane);
+gui.add(world.plane, 'heightSegments', 1, 50).onChange(generatePlane);
+
+function generatePlane() {
+    planeMesh.geometry.dispose();
+    planeMesh.geometry = new THREE.PlaneGeometry(
+        world.plane.width,
+        world.plane.height,
+        world.plane.widthSegments,
+        world.plane.heightSegments
+    );
+
+    const {array} = planeMesh.geometry.attributes.position;
+    for (let i = 0; i < array.length; i += 3) {
+        const x = array[i];
+        const y = array[i + 1];
+        const z = array[i + 2];
+        
+        array[i + 2] = z + Math.random();
+    }
+}
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
@@ -11,7 +46,7 @@ document.body.appendChild(renderer.domElement);
 
 camera.position.z = 5;
 
-const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
     color: 0xff0000,
     side: THREE.DoubleSide,
